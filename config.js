9a2c6606bf18a596465d903297522aadff571dd2
Laquanday <89256340+Laquanday@users.noreply.github.com>
2021-08-20 19:38:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
+    introTitle: 'Người yêu em ơi!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    title: 'Có phải anh rất yêu em đúng không?',
+    desc: 'Phải trả lời hết, đừng có thoát ra đó! ',
+    btnYes: 'Anh yêu em rất nhiều!',
+    btnNo: 'Ừ anh yêu em',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu em',
+    btnReply: 'Gửi cho em',
+    reply: 'Vì em có sức hút đặc biệt',
+    mess: 'Embi ết mà. Em cũng yêu anh.',
+    messDesc: 'Ting của em, đồng ý làm vợ em nhé?',
+    btnAccept: 'Anh đồng ý!',
+    messLink: 'https://www.facebook.com/CuuTuyHuan/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
